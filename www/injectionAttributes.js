af0794a8c567bccfe4be27a126785defd2140767
rahulbhobe <rahulbhobe@yahoo.com>
2015-08-20 02:49:35 -0400

    Showed sphere (not cone) at injection location.

--- www/injectionAttributes.js
+++ www/injectionAttributes.js
@@ -7,6 +7,30 @@ var InjectionPoint = function(obj){
 	Autodesk.ADN.Viewing.Extension.UIComponent.panelInstance.addPoint(this);
 };
 
+InjectionPoint.prototype._getBoundingBoxDiagonal = function(viewer) {
+  var boundingBox = viewer.model.getBoundingBox();
+  var maxpt = boundingBox.max;
+  var minpt = boundingBox.min;
+
+  var xdiff = maxpt.x - minpt.x;
+  var ydiff = maxpt.y - minpt.y;
+  var zdiff = maxpt.z - minpt.z;
+
+  return Math.pow((xdiff * xdiff + ydiff * ydiff + zdiff * zdiff), 0.5);
+}
+
+InjectionPoint.prototype.createGeometry = function(viewer) {
+  var material = new THREE.MeshPhongMaterial({ color: 0xff0000 });
+  viewer.impl.matman().addMaterial('MaterialForInjectionPoint', material, true);
+
+  var sphereRadius = this._getBoundingBoxDiagonal(viewer) / 50;
+  var sphere = new THREE.Mesh(new THREE.SphereGeometry(sphereRadius, 20), material);
+  sphere.position.set(this.location.x, this.location.y, this.location.z);
+
+  viewer.impl.scene.add(sphere);
+  viewer.impl.invalidate(true);
+};
+
 InjectionPoint.prototype.attachToGeometry = function(){
 
 };
@@ -37,7 +61,7 @@ var InjectionManager = function(viewer){
 		}
 
 		console.log('good');
-		self.add(location);
+		self.add(location, viewer);
 	});
 
 	this.all = [];
@@ -49,6 +73,8 @@ InjectionManager.prototype.handleSelection = function(e){
 	// console.log(this.all);
 };
 
-InjectionManager.prototype.add = function(location){
-	this.all.push(new InjectionPoint({location: location}));
+InjectionManager.prototype.add = function(location, viewer) {
+  var injectionPoint = new InjectionPoint({location: location});
+  injectionPoint.createGeometry(viewer);
+  this.all.push(injectionPoint);
 }
